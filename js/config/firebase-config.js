e2c3698c6017c31ce995eac572a0918a30f696f2
JMBYTGAMER <jobinmathewbiju6@gmail.com>
2026-09-14 16:34:02 +0000

    Improve JMB HUB SEO

--- js/config/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDT0FFll2NkMYiam0_Sbct5P52AqF_Cnpk",
-      authDomain: "jmb-hub.firebaseapp.com",
-        projectId: "jmb-hub",
-          storageBucket: "jmb-hub.firebasestorage.app",
-            messagingSenderId: "59308977494",
-              appId: "1:59308977494:web:f5149968e5ae94d5903af7",
-                measurementId: "G-ED3Z0XQY5T"
-                };
